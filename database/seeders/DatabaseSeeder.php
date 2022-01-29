d364f7d41d811b0271e685cc8624b2df57c8ab20
adan sanchez <37454169+adan-sanchez-luis@users.noreply.github.com>
2022-01-28 18:56:49 -0600

    seeders de proveedores

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,17 +17,6 @@ class DatabaseSeeder extends Seeder
         $this->call(RoleSeeder::class);
         $this->call(UserSeeder::class);
         $this->call(ClienteSeeder::class);
-
-
-        //$Rol1 = new Rol_Empleado();
-        //$Rol1->descripcion = "Supervisor";
-        //$Rol1->salario = 2000;
-
-        //$Rol2 = new Rol_Empleado();
-        //$Rol2->descripcion = "Empleado";
-        //$Rol2->salario = 1200;
-
-        //$Rol1->save();
-        //$Rol2->save();
+        $this->call(ProveedorSeeder::class);
     }
 }
